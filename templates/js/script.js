af9a581af661c2beb8262548fa07120a3bf74092
Faruk <109393466+farukbeygo@users.noreply.github.com>
2023-03-30 16:05:08 +0300

    Delete templates directory

--- templates/js/script.js
+++
@@ -1,9 +0,0 @@
-$(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-    if (scroll > 0) {
-      $(".navbar").addClass("transparent");
-    } else {
-      $(".navbar").removeClass("transparent");
-    }
-});
-  
